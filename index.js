7fa941d2f0ee4a2cea859c4ad192e81263475425
Jorge Callisaya <sanchezenrique580@gmail.com>
2021-02-19 23:25:22 -0400

    Creacion de metodos para obtener un curso y otro para obtener varios cursos segun el tema

--- index.js
+++ index.js
@@ -3,17 +3,48 @@ const app = express();
 const { graphqlHTTP } = require('express-graphql');
 const { buildSchema } = require('graphql');
 
+// Data
+const { courses }=require('./data.json');
 
 // Establecemos el tipo de consulta que se puede realizar
 const schema = buildSchema(`
     type Query{
-        message:String
+        course(id:Int!):Course
+        courses(topic:String):[Course]
+    }
+    type Course{
+        id:Int
+        title:String
+        author:String
+        topic:String
+        url:String
     }
 `);
+
+let getCourse =(args)=>{
+    let id = args.id;
+    return courses.filter(course=>{
+        return course.id==id;
+    })[0]
+}
+
+let getCourses =(args)=>{
+    if(args.topic){
+        let topic = args.topic;
+        return courses.filter(course=>{
+            return course.topic===topic;
+        })
+    }else{
+        return courses;
+    }
+
+}
 
 // Sirve para establecer a que puede consultar desde nuestros datos
 const root = {
     message:()=>'hello world',
+    course:getCourse,
+    courses:getCourses
 }
 
 // Creacion de una ruta unica para obtener los datos
